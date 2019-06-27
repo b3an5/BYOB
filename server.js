9abcc593947fa1a58f3397ade1f3c8fc111e5904
B3an5 <beantheo@gmail.com>
2019-06-27 14:46:10 -0600

    post methods

--- server.js
+++ server.js
@@ -68,3 +68,43 @@ app.get("/api/v1/people/:id", (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.post("/api/v1/heroes", (request, response) => {
+  const { name, universe, location } = request.body;
+  const hero = request.body;
+
+  if (!name || !universe || !location) {
+    return response
+      .status(422)
+      .json("please provide a name, universe and location for hero");
+  } else {
+    database("heroes")
+      .insert(hero, "id")
+      .then(hero => {
+        response.status(201).json({ id: hero[0] });
+      })
+      .catch(error => {
+        response.status(500).json({ error });
+      });
+  }
+});
+
+app.post("/api/v1/people", (request, response) => {
+  const { name, hero_id } = request.body;
+  const person = request.body;
+
+  if (!name || !hero_id) {
+    return response
+      .status(422)
+      .json("please provide a name and the coresponding hero id");
+  } else {
+    database("people")
+      .insert(person, "id")
+      .then(person => {
+        response.status(201).json({ id: person[0] });
+      })
+      .catch(error => {
+        response.status(500).json({ error });
+      });
+  }
+});
