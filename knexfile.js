c95fd3e24787880a00cf8ed2c37bba5d5cca4c3d
B3an5 <beantheo@gmail.com>
2019-06-28 09:53:15 -0600

    added production

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,14 @@
 // Update with your config settings.
 
 module.exports = {
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: "./db/migrations"
+    },
+    useNullAsDefault: true
+  },
   development: {
     client: "pg",
     connection: "postgres://localhost/superheroes",
