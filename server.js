320f5a6b76c5cfc5191a3f909ff71f12829a9759
B3an5 <beantheo@gmail.com>
2019-06-28 09:56:52 -0600

    moved const in server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const configuration = require("./knexfile")[environment];
 const environment = process.env.NODE_ENV || "development";
+const configuration = require("./knexfile")[environment];
 const database = require("knex")(configuration);
 const app = express();
 const PORT = process.env.PORT || 3000;
